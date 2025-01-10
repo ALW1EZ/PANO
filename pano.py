4bc80cdbc7341135ecc45fe571b2efb57a15098c
ALW1EZ <kombaktotos@gmail.com>
2025-01-10 14:13:27 +0300

    Update version to 5.0.0, remove unused transforms, and streamline codebase
    - Updated the application version in `pano.py` from 4.9.4 to 5.0.0.
    - Deleted the `ReverseImage` and `UsernameToWebsite` transforms, simplifying the codebase by removing unused functionality.
    
    These changes enhance versioning and improve code maintainability by eliminating obsolete components.

--- pano.py
+++ pano.py
@@ -77,7 +77,7 @@ class DateTimeEncoder(json.JSONEncoder):
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.version = "4.9.4"
+        self.version = "5.0.0"
         self.setWindowTitle(f"PANO - Platform for Analysis and Network Operations | v{self.version}")
         self.selected_entity = None
         self.current_file = None
